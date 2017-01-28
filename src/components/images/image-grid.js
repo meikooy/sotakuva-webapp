863d1e55a42fde59ad7c6272d99fd818841c2c95
mikkokut <mikko93@gmail.com>
2017-01-29 00:34:31 +0200

    fix

--- src/components/images/image-grid.js
+++ src/components/images/image-grid.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 
 
-const getImgStyle = img =>
-  ({backgroundImage: `url(https://api.rintamalla.fi/images/${img.objectID}/file?size=thumbnail)`});
+const getImgStyle = img => ({
+  background:
+  `url("https://api.rintamalla.fi/images/${img.objectID}/file?size=thumbnail") center center / cover no-repeat, `
+  + 'url("/images/loader.gif") center center / 80px auto no-repeat'
+});
 
 
 export default class Images extends Component {
