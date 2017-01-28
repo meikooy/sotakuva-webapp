8efb9b8d6fe584933f7365b4c96433e6c777eb2d
Aleksi Hynönen <aleksi.hynonen@meiko.fi>
2017-01-28 23:38:27 +0200

    front page

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,29 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import cn from 'classnames';
 
-export default class Home extends Component {
+export default class Welcome extends Component {
   render() {
-    return <p>TODO</p>;
+    return (
+      <div>
+        <div className="items">
+          <div>
+            <img src='https://sotakuva-api.herokuapp.com/images/588c8ce5dfa87d48afea2bd7/file?size=large' alt='' className="img-setting"/>
+          </div>
+          <div className='front-text-panel'><h3 >Talvisota</h3></div>
+        </div>
+        <div className="items">
+          <div>
+            <img src='https://sotakuva-api.herokuapp.com/images/588cb5899b350e5298dbfefe/file?size=large' alt='' className="img-setting"/>
+          </div>
+          <div className='front-text-panel'><h3>Jatkosota</h3></div>
+        </div>
+        <div className="items">
+          <div>
+            <img src='https://sotakuva-api.herokuapp.com/images/588ce3860995b179635014c7/file?size=large' alt='' className="img-setting"/>
+          </div>
+          <div className="front-text-panel"><h3>Lapin sota</h3></div>
+        </div>
+      </div>
+    );
   }
 }
