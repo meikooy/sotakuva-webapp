4737b4cd51c70af403649613c7f9ad9c0efe06a3
Mikko Kokkoniemi <mikko.kokkoniemi@meiko.fi>
2017-01-28 14:46:16 +0200

    images & home container & page

--- src/routes/large.js
+++ src/routes/large.js
@@ -4,6 +4,7 @@ import {Route, IndexRedirect, IndexRoute, Redirect, createRoutes} from 'react-ro
 import RootPage from '../pages/root';
 import HomePage from '../pages/home-page';
 import SearchPage from '../pages/search-page';
+import ImagesPage from '../pages/images-page';
 
 
 export default (
@@ -11,6 +12,7 @@ export default (
     <IndexRoute component={HomePage} />
 
     <Route name='images' path='images'>
+      <IndexRoute component={ImagesPage} />
     </Route>
 
     <Route name='search' path='haku' component={SearchPage} />
